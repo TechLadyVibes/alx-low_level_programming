e5dd6d406c2c8ad451b8f75e709e232a9cd18dea
TechLadyVibes <mohphethic@gmail.com>
2022-10-19 15:04:08 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/**
+ * main - print _putchar
+ * is written by bwave
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('h');
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+project Functions and nested loop
